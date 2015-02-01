f13816a40d41d9422cf29726bf037df1ae3beec4
ttagawa <tylertagawa@gmail.com>
2015-01-31 19:18:54 -0800

    Got food to randomize position

--- main.js
+++ main.js
@@ -2,18 +2,23 @@ use2D = true;
 var size = 10;
 var snakeArray=[];
 var score = 0;
-var food;
+var length=2;
 
 function buildSn(){
-	var length = 2;
 	for(var i=length;i>0;i--){
 		snakeArray.push({x:i,y:0});
 	}
 }
+var food = new Sprite();
+food.x=size*Math.round(Math.random()*(800/size));
+food.y=food.x=size*Math.round(Math.random()*(600/size));;
+food.width=size;
+food.height=size;
+food.image=Textures.load("https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRaVNqGdvSMitbimeaVzUnxRmIUjCCc_4Jq-AIAOu_YY0-FkL9b");
+world.addChild(food);
 function moveFood(){
-	food={x:Math.round(Math.random()*(canvas.width-size)/size),
-		y:Math.round(Math.random()*(canvas.height-size)/size)
-	};
+	food.x=size*Math.round(Math.random()*(Canvas.width-size)/size);
+	food.y=size*Math.round(Math.random()*(canvas.height-size)/size);
 }
 function drawSnake(){
 	var canvas = document.getElementById('canvas');
@@ -22,5 +27,8 @@ function drawSnake(){
 		ctx.fillRect(snakeArray[i].x,snakeArray[i].y,size,size);
 	}
 }
+/*world.update=function(d){
+	drawSnake();
+}
 buildSn();
-drawSnake();
+drawSnake();*/
